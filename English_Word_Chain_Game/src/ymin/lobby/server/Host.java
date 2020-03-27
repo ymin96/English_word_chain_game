4ce714b512ca2e6c7cfaaf6199b89b3ef9481c87
Kim YongHwan <ymin96@naver.com>
2020-03-27 13:14:03 +0900

    Add Host Object

---
+++ English_Word_Chain_Game/src/ymin/lobby/server/Host.java
@@ -0,0 +1,13 @@
+package ymin.lobby.server;
+
+public class Host {
+	String ipAddress;
+	int portNumber;
+	
+	Host(String ipAddress, int portNumber){
+		this.ipAddress = ipAddress;
+		this.portNumber = portNumber;
+	}
+	
+	
+}
--- English_Word_Chain_Game/src/ymin/lobby/server/Server.java
+++ English_Word_Chain_Game/src/ymin/lobby/server/Server.java
@@ -1,10 +1,14 @@
 package ymin.lobby.server;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -12,9 +16,9 @@ import java.util.Vector;
 
 public class Server {
 	ServerSocketChannel serverSocketChannel;
-	public static Selector selector;
+	public Selector selector;
 	List<User> connections = new Vector<User>();
-	
+	Map<String, String> 
 	public void startServer() {
 		try {
 			selector = Selector.open();
@@ -37,6 +41,7 @@ public class Server {
 					if(keyCount == 0) { continue;}
 					
 					Set<SelectionKey> selectedKeys = selector.selectedKeys();
+					
 					Iterator<SelectionKey> iterator = selectedKeys.iterator();
 					while(iterator.hasNext()) {
 						SelectionKey key = iterator.next();
@@ -52,6 +57,7 @@ public class Server {
 						}
 						iterator.remove();
 					}
+					
 				}catch(Exception e){
 					if(serverSocketChannel.isOpen()) {
 						stopServer();
@@ -60,7 +66,7 @@ public class Server {
 				}
 			}
 		});
-		
+		 
 		thread.start();
 	}
 	
@@ -79,6 +85,7 @@ public class Server {
 		} catch(Exception e) {}
 	}
 	
+	//클라이언트 연결 수락
 	private void accept(SelectionKey key) {
 		try {
 			ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
@@ -90,6 +97,50 @@ public class Server {
 			connections.add(user);
 			
 		} catch(Exception e) {
+			
+		}
+	}
+
+	class User {
+		protected SocketChannel socketChannel;
+		public String sendData;
+		
+		User(SocketChannel socketChannel) throws IOException{
+			this.socketChannel = socketChannel;
+			socketChannel.configureBlocking(false);
+			SelectionKey selectionKey = socketChannel.register(selector, SelectionKey.OP_READ);
+			selectionKey.attach(this);
+		}
+		
+		void receive(SelectionKey selectionKey) {
+			try {
+				ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+				
+				//데이터 받기
+				int byteCount = socketChannel.read(byteBuffer);
+				
+				//상대방이 SokcetChannel의 close() 메소드를 호출할 경우
+				if(byteCount == -1)
+					throw new IOException();
+				
+				//받은 데이터 디코드 
+				byteBuffer.flip();
+				Charset charset = Charset.forName("UTF-8");
+				String data = charset.decode(byteBuffer).toString();
+				
+				switch (Integer.parseInt(data)) {
+				case 1:		//생성된 방 조회
+					
+					break;
+				default:
+					throw new IllegalArgumentException("Unexpected value: " + Integer.parseInt(data));
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+		void send(SelectionKey selectionKey) {
 			
 		}
 	}
